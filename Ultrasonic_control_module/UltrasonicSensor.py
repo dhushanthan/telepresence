e53a0b91341e3cafe70be72dc4f555c931f48c63
barath2905 <92852071+barath2905@users.noreply.github.com>
2021-11-18 15:01:29 +0530

    ultrasonic new change

--- Ultrasonic_control_module/UltrasonicSensor.py
+++ Ultrasonic_control_module/UltrasonicSensor.py
@@ -10,13 +10,13 @@ while True:
     GPIO.output(TRIG,False)
     print("waiting for sensor to settle")
     time.sleep(0.2)
-    GPIO.output(TRIG.True)
+    GPIO.output(TRIG,True)
     time.sleep(0.00001)
     GPIO.output(TRIG,False)
     while GPIO.input(ECHO) == 0:
-        pulse start = time.time()
+        pulse_start = time.time()
     while GPIO.input(ECHO)==1:
-        pulse end=time.time()
+        pulse_end=time.time()
     pulse_duration = pulse_end-pulse_start
     distance=pulse_duration*17150
     distance=round(distance,2)
