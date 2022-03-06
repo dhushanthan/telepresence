76cc31224fa30d659399122fdc97d3e2f954b0b9
dhushanthan <sdhushanthan052@gmail.com>
2022-03-06 19:09:03 +0530

    Testing

--- tests/test_conference.py
+++ tests/test_conference.py
@@ -7,8 +7,9 @@ import threading
 local_ip_address = socket.gethostbyname(socket.gethostname())
 print(local_ip_address)
 
-server = StreamingServer(local_ip_address, 9999)
-receiver = AudioReceiver(local_ip_address, 8888)
+server = StreamingServer(local_ip_address, 7777)
+receiver = AudioReceiver(local_ip_address, 6666)
+
 
 def start_listening():
     t1 = threading.Thread(target=server.start_server)
@@ -16,24 +17,26 @@ def start_listening():
     t1.start()
     t2.start()
 
+
 def start_camera_stream():
-    camera_client = CameraClient(text_target_ip.get(1.0, 'end-1c'), 7777)
+    camera_client = CameraClient(text_target_ip.get(1.0, 'end-1c'), 9999)
     t3 = threading.Thread(target=camera_client.start_stream)
     t3.start()
 
 
 def start_screen_sharing():
-    stream_client = ScreenShareClient(text_target_ip.get(1.0, 'end-1c'), 7777)
+    stream_client = ScreenShareClient(text_target_ip.get(1.0, 'end-1c'), 9999)
     t4 = threading.Thread(target=stream_client.start_stream)
     t4.start()
 
 
 def start_audio_stream():
-    audio_sender = AudioSender(text_target_ip.get(1.0, 'end-1c'), 6666)
+    audio_sender = AudioSender(text_target_ip.get(1.0, 'end-1c'), 8888)
     t5 = threading.Thread(target=audio_sender.start_stream)
     t5.start()
 
 # GUI
+
 
 window = tk.Tk()
 window.title("Telepresence Conference Testing")
@@ -45,16 +48,20 @@ label_target_ip.pack()
 text_target_ip = tk.Text(window, height=1)
 text_target_ip.pack()
 
-btn_listen = tk.Button(window, text="Start Listening", width=50, command=start_listening)
+btn_listen = tk.Button(window, text="Start Listening",
+                       width=50, command=start_listening)
 btn_listen.pack(anchor=tk.CENTER, expand=True)
 
-btn_camera = tk.Button(window, text="Start Camera Stream", width=50, command=start_camera_stream)
+btn_camera = tk.Button(window, text="Start Camera Stream",
+                       width=50, command=start_camera_stream)
 btn_camera.pack(anchor=tk.CENTER, expand=True)
 
-btn_screen = tk.Button(window, text="Start Screen Sharing", width=50, command=start_screen_sharing)
+btn_screen = tk.Button(window, text="Start Screen Sharing",
+                       width=50, command=start_screen_sharing)
 btn_screen.pack(anchor=tk.CENTER, expand=True)
 
-btn_audio = tk.Button(window, text="Start Audio Stream", width=50, command=start_audio_stream)
+btn_audio = tk.Button(window, text="Start Audio Stream",
+                      width=50, command=start_audio_stream)
 btn_audio.pack(anchor=tk.CENTER, expand=True)
 
 window.mainloop()
