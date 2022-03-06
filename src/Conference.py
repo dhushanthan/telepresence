fbf9e0067193ea067f95e95c93b41fc03319b05b
dhushanthan <sdhushanthan052@gmail.com>
2022-03-06 18:52:55 +0530

    test phase 1

--- src/Conference.py
+++ src/Conference.py
@@ -2,3 +2,36 @@ from vidstream import *
 import tkinter as tk
 import socket
 import threading
+import requests
+
+
+local_ip_address = socket.gethostbyname(socket.gethostname())
+print(local_ip_address)
+
+#server = StreamServer(local_ip_address, 8888)
+
+# GUI
+
+window = tk.Tk()
+window.title("Telepresence Conference Testing")
+window.geometry('300x200')
+
+label_target_ip = tk.Label(window, text="Target IP:")
+label_target_ip.pack()
+
+text_target_ip = tk.Text(window, height=1)
+text_target_ip.pack()
+
+btn_listen = tk.Button(window, text="Start Listening", width=50)
+btn_listen.pack(anchor=tk.CENTER, expand=True)
+
+btn_camera = tk.Button(window, text="Start Camera Stream", width=50)
+btn_camera.pack(anchor=tk.CENTER, expand=True)
+
+btn_screen = tk.Button(window, text="Start Screen Sharing", width=50)
+btn_screen.pack(anchor=tk.CENTER, expand=True)
+
+btn_audio = tk.Button(window, text="Start Audio Stream", width=50)
+btn_audio.pack(anchor=tk.CENTER, expand=True)
+
+window.mainloop()
