8f8029e9f1b42e4ba0680e17e883b2cadb11e950
dhushanthan <sdhushanthan052@gmail.com>
2022-03-22 19:38:18 +0530

    new update

--- src/Motor_control.py
+++ src/Motor_control.py
@@ -1,5 +1,9 @@
 import curses
 import RPi.GPIO as GPIO
+import pygame
+
+pygame.init()
+display = pygame.display.set_mode((300, 300))
 
 #set GPIO numbering mode and define output pins
 GPIO.setmode(GPIO.BCM)
@@ -17,37 +21,44 @@ screen.keypad(True)
 print("Controls: \n1. UP arrow - move forward\n2. DOWN arrow - move backward\n3. Right arrow - Turn right\n4. Left arrow - Turn left")
 
 try:
-    while True:   
-        char = screen.getch()
-        if char == ord('q'):
-            break
-        elif char == curses.KEY_UP:
-            GPIO.output(24,False)
-            GPIO.output(23,True)
-            GPIO.output(22,False)
-            GPIO.output(17,True)
-        elif char == curses.KEY_DOWN:
-            GPIO.output(24,True)
-            GPIO.output(17,False)
-            GPIO.output(22,True)
-            GPIO.output(23,False)
-        elif char == curses.KEY_RIGHT:
-            GPIO.output(24,False)
-            GPIO.output(22,True)
-            GPIO.output(23,True)
-            GPIO.output(17,False)
-        elif char == curses.KEY_LEFT:
-            GPIO.output(24,True)
-            GPIO.output(22,False)
-            GPIO.output(23,False)
-            GPIO.output(17,True)
-        elif char == 10:
-            GPIO.output(24, False)
-            GPIO.output(23, False)
-            GPIO.output(22, False)
-            GPIO.output(17, False)
+    run = True
+    while run:   
+        for event in pygame.event.get():
+            if event.type == pygame.K_END:
+                run = False
+                pygame.display.quit()
+                pygame.quit()
+                exit()
 
-        char = 10
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    GPIO.output(24,False)
+                    GPIO.output(23,True)
+                    GPIO.output(22,False)
+                    GPIO.output(17,True)
+                if event.key == pygame.K_DOWN:
+                    GPIO.output(24,True)
+                    GPIO.output(17,False)
+                    GPIO.output(22,True)
+                    GPIO.output(23,False)
+                if event.key == pygame.K_RIGHT:
+                    GPIO.output(24,False)
+                    GPIO.output(22,True)
+                    GPIO.output(23,True)
+                    GPIO.output(17,False)
+                if event.key == pygame.K_LEFT:
+                    GPIO.output(24,True)
+                    GPIO.output(22,False)
+                    GPIO.output(23,False)
+                    GPIO.output(17,True)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    GPIO.output(24, False)
+                    GPIO.output(23, False)
+                    GPIO.output(22, False)
+                    GPIO.output(17, False)
+
+
              
 finally:
     #Close down curses properly, inc turn echo back on!
